000f3a2d107e45ea33a135eb703050b62818a83e
rusou22 <KTC23A31C0003@edu.kyoto-tech.ac.jp>
2023-09-08 06:41:37 +0900

    aaa

--- これ/script.js
+++ これ/script.js
@@ -1,16 +1,3 @@
-$(nav).on("click", function () {
-  var $el = $(this),
-    id = $el.attr("href");
-  $("html, body").animate(
-    {
-      scrollTop: $(id).offset().top - headerH,
-    },
-    500
-  );
-  if (winW < 890) {
-    $("#menuWrap").next().slideToggle();
-    $("#menuBtn").removeClass("close");
-  }
-  return false;
+bubbly({
+  bubbles: {},
 });
-
---
+++ 作品/otoiawase/script.js
@@ -0,0 +1,13 @@
+script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const contactForm = document.getElementById("contact-form");
+  contactForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(contactForm);
+    alert("お問い合わせが送信されました！");
+  });
+});
+
+bubbly({
+  bubbles: {},
+});
